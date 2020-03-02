723094a081b851726bbf4f715c0ec89e58addeae
StanR <hi@stanr.info>
2020-03-02 17:08:30 +0300

    Add top_full generator, update top_full

---
+++ FullTopBuilder/PlayerModel.cs
@@ -0,0 +1,14 @@
+﻿
+namespace FullTopBuilder
+{
+    public class PlayerModel
+    {
+        public string Username { get; set; }
+        public string LivePP { get; set; }
+        public string LocalPP { get; set; }
+        public double LocalPPNumeric { get; set; }
+        public string PPChange { get; set; }
+
+        public override string ToString() => Username;
+    }
+}
