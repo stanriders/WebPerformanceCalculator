4a47abb8ec4be283fbbb9a1c64860680fe76607b
StanR <hi@stanr.info>
2020-07-02 12:44:39 +0300

    Add highscore_full builder

---
+++ HighscoreBuilder/ScoreModel.cs
@@ -0,0 +1,10 @@
+﻿
+namespace HighscoreBuilder
+{
+    public class ScoreModel
+    {
+        public string Player { get; set; }
+        public string Beatmap { get; set; }
+        public double LocalPP { get; set; }
+    }
+}
