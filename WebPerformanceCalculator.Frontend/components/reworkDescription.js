e85ab112701db858213168a10b30ad997212d738
StanR <hi@stanr.info>
2022-04-27 15:31:40 +0300

    Add feedback form

--- WebPerformanceCalculator.Frontend/components/reworkDescription.js
+++ WebPerformanceCalculator.Frontend/components/reworkDescription.js
@@ -1,5 +1,7 @@
 
+import Alert from 'react-bootstrap/Alert'
 import Card from 'react-bootstrap/Card'
+import Link from 'next/link'
 import CurrentBuild from '../components/currentBuild'
 import consts from '../consts'
 
@@ -11,7 +13,11 @@ export default function ReworkDescription() {
           <Card.Text>
             <p><CurrentBuild /></p>
             <div dangerouslySetInnerHTML={{
-                  __html: consts.description}} /></Card.Text>
+                  __html: consts.description}} />
+            <Alert variant="warning">
+              Please give the feedback about this rework through <b><Link href="https://forms.gle/qQMCscj6vYVc6uVh6">this form</Link></b>, we would really appreciate it!
+            </Alert>
+          </Card.Text>
         </Card.Body>
       </Card>
     </>
