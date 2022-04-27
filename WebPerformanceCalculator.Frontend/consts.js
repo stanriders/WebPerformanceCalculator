851baeca3f0155f98f0dddb2c061aba8040753b1
StanR <hi@stanr.info>
2022-04-27 16:04:20 +0300

    Actually Nevermind

--- WebPerformanceCalculator.Frontend/consts.js
+++ WebPerformanceCalculator.Frontend/consts.js
@@ -1,6 +1,6 @@
 module.exports = { 
     title: "Shannon Microflow rework",
-    link: "https://github.com/stanriders/WebPerformanceCalculator/blob/funny/WebPerformanceCalculator/Services/CalculationService.cs#L235",
+    link: "https://github.com/ppy/osu",
     description: "<p>Keep in mind that this is <b>NOT</b> final result and its not meant to represent what rebalance will look like when it's done. It is yet unknown when it will be done.</p>\
     <p>Documentation is WIP. If you have any questions about this rework you can ask them in <a href=\"https://discord.gg/aqPCnXu\">PP Development Discord</a>.</p>"
 }
